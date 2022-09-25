e80b6d926bf0b7e96ed3ac39531305265f2dd9d8
PatrickD3003 <dharmapatrick88@gmail.com>
2022-09-25 23:19:54 +0900

    add ogre, correct movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import os
 from tower import Tower
 from soldier import Soldier
+
 # initialize pygame
 pygame.init()
 
@@ -33,20 +35,22 @@ right_tower = Tower(R_TOWER_WIDTH, R_TOWER_HEIGHT, R_TOWER_X, R_TOWER_Y, RED)
 
 # create soldier object
 # left side soldier
-L_SOLDIER_WIDTH = 10
-L_SOLDIER_HEIGHT = 20
+L_SOLDIER_WIDTH = 40
+L_SOLDIER_HEIGHT = 40
 L_SOLDIER_X = 20 + L_TOWER_WIDTH
 L_SOLDIER_Y = HEIGHT - L_SOLDIER_HEIGHT
 L_SOLDIER_COLOR = GREEN
 L_SOLDIER_VELOCITY = 5
+L_SOLDIER_ROTATE = False
 
 # right side soldier
-R_SOLDIER_WIDTH = 10
-R_SOLDIER_HEIGHT = 20
+R_SOLDIER_WIDTH = 40
+R_SOLDIER_HEIGHT = 40
 R_SOLDIER_X = WIDTH - 20 - R_TOWER_WIDTH - R_SOLDIER_WIDTH
 R_SOLDIER_Y = HEIGHT - R_SOLDIER_HEIGHT
 R_SOLDIER_COLOR = BLUE
 R_SOLDIER_VELOCITY = -5
+R_SOLDIER_ROTATE = True
 
 
 
@@ -65,10 +69,10 @@ def draw_window(left_soldiers_barrack, right_soldiers_barrack):
 
 def handle_movement(left_soldiers_barrack, right_soldiers_barrack):
         for left_soldier in left_soldiers_barrack:
-            left_soldier.create_soldier.x += L_SOLDIER_VELOCITY
+            left_soldier.x += L_SOLDIER_VELOCITY
 
         for right_soldier in right_soldiers_barrack:
-            right_soldier.create_soldier.x += R_SOLDIER_VELOCITY
+            right_soldier.x += R_SOLDIER_VELOCITY
 
 def main():
     run = True
@@ -86,10 +90,10 @@ def main():
 
             if event.type == pygame.KEYDOWN: 
                 if event.key == pygame.K_1:  # summon left side soldier
-                    left_soldier = Soldier(L_SOLDIER_WIDTH, L_SOLDIER_HEIGHT, L_SOLDIER_X, L_SOLDIER_Y, L_SOLDIER_COLOR)
+                    left_soldier = Soldier(L_SOLDIER_WIDTH, L_SOLDIER_HEIGHT, L_SOLDIER_X, L_SOLDIER_Y, L_SOLDIER_COLOR, L_SOLDIER_ROTATE)
                     left_soldiers_barrack.append(left_soldier)
                 if event.key == pygame.K_LEFT:  # summon right side soldier
-                    right_soldier = Soldier(R_SOLDIER_WIDTH, R_SOLDIER_HEIGHT, R_SOLDIER_X, R_SOLDIER_Y, R_SOLDIER_COLOR)
+                    right_soldier = Soldier(R_SOLDIER_WIDTH, R_SOLDIER_HEIGHT, R_SOLDIER_X, R_SOLDIER_Y, R_SOLDIER_COLOR, R_SOLDIER_ROTATE)
                     right_soldiers_barrack.append(right_soldier)
 
         handle_movement(left_soldiers_barrack, right_soldiers_barrack)
--- soldier.py
+++ soldier.py
@@ -1,18 +1,24 @@
 import pygame
+import os
+WIDTH, HEIGHT = 900, 500
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 class Soldier:
-    WIDTH, HEIGHT = 900, 500
-    WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-    def __init__(self, soldier_width, soldier_height, soldier_x, soldier_y, soldier_color):
+    def __init__(self, soldier_width, soldier_height, soldier_x, soldier_y, soldier_color, soldier_rotate):
         self.width = soldier_width
         self.height = soldier_height
         self.x = soldier_x
         self.y = soldier_y
         self.color = soldier_color
+        self.rotate = soldier_rotate
         self.create_soldier = pygame.Rect(self.x, self.y, self.width, self.height)
-    
+        self.image_soldier_import = pygame.image.load(os.path.join("characters", "ogre_sprite.png"))
+        if self.rotate == True:
+            self.image_soldier = pygame.transform.flip(pygame.transform.scale(self.image_soldier_import, (self.width, self.height)), True, False)
+        else:
+            self.image_soldier = pygame.transform.scale(self.image_soldier_import, (self.width, self.height))
         
     def summon_soldier(self):
-        pygame.draw.rect(self.WINDOW, self.color, self.create_soldier)
+        WINDOW.blit(self.image_soldier, (self.x, self.y))
 
     def soldier_move(self, soldier_velocity):
         self.velocity = soldier_velocity
