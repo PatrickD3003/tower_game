41788ab3d7b6c3e473a9e6fb33f71bf87f9f0985
NathanaelIanGunadi <38781078+NathanaelIanGunadi@users.noreply.github.com>
2022-09-25 19:32:34 +0900

    Add files via upload

--- soldier.py
+++ soldier.py
@@ -1,25 +1,26 @@
 import pygame
+
+pygame.init()
+
 class Soldier:
-    WIDTH, HEIGHT = 900, 500
-    WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-    def __init__(self, soldier_width, soldier_height, soldier_x, soldier_y, soldier_color):
-        self.width = soldier_width
-        self.height = soldier_height
-        self.x = soldier_x
-        self.y = soldier_y
-        self.color = soldier_color
-        self.create_soldier = pygame.Rect(self.x, self.y, self.width, self.height)
-        
-    def summon_soldier(self):
-        return pygame.draw.rect(self.WINDOW, self.color, self.create_soldier)
 
-    def soldier_move(self, soldier_velocity):
-        self.velocity = soldier_velocity
-        self.create_soldier.x += self.velocity
+    def __init__(self, img, team, name):
+        w, h = pygame.display.get_surface().get_size()
+        self.img = pygame.image.load(img)
+        self.img = pygame.transform.scale(self.img, (50, 50))
+        self.team = team
+        self.name = name
+        self.y = h - self.img.get_height()
+        if team == "1":
+            self.x = 100 - (self.img.get_width() / 2)
+        elif team == "2":
+            self.img = pygame.transform.flip(self.img, True, False)
+            self.x = w - 100 - (self.img.get_width() / 2)
 
-        
-
-    
-    
-
+    def move(self):
+        if self.team == "1":
+            self.x += 2
+        elif self.team == "2":
+            self.x -= 2
+        return self.x, self.y
 
