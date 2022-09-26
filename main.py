69ae1226a5abc216cb51f3658b14f908e904eb20
PatrickD3003 <111837275+PatrickD3003@users.noreply.github.com>
2022-09-26 23:39:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,27 @@
 import pygame
+import os
 from tower import Tower
 from soldier import Soldier
+
 # initialize pygame
 pygame.init()
 
+right_exist = False
+left_exist = False
 # initial setups
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1200, 500
 FPS = 60
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tower Game")
-
+ROTATE = True  # right team
+NO_ROTATE = False  # left team
 # basic colors rgb black white
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+WHITE, BLACK, RED, GREEN, BLUE = (255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255)
 
-# create tower object
-# left side tower
-L_TOWER_WIDTH = 100
-L_TOWER_HEIGHT = 200
-L_TOWER_X = 20
-L_TOWER_Y = HEIGHT - L_TOWER_HEIGHT
-left_tower = Tower(L_TOWER_WIDTH, L_TOWER_HEIGHT, L_TOWER_X, L_TOWER_Y, RED)
-# right side tower
-R_TOWER_WIDTH = 100
-R_TOWER_HEIGHT = 200
-R_TOWER_X = WIDTH - R_TOWER_WIDTH - 20
-R_TOWER_Y = HEIGHT - R_TOWER_HEIGHT
-right_tower = Tower(R_TOWER_WIDTH, R_TOWER_HEIGHT, R_TOWER_X, R_TOWER_Y, RED)
+velocity = 2
 
-# create soldier object
-# left side soldier
-L_SOLDIER_WIDTH = 10
-L_SOLDIER_HEIGHT = 20
-L_SOLDIER_X = 20 + L_TOWER_WIDTH
-L_SOLDIER_Y = HEIGHT - L_SOLDIER_HEIGHT
-L_SOLDIER_COLOR = GREEN
-L_SOLDIER_VELOCITY = 5
-
-# right side soldier
-R_SOLDIER_WIDTH = 10
-R_SOLDIER_HEIGHT = 20
-R_SOLDIER_X = WIDTH - 20 - R_TOWER_WIDTH - R_SOLDIER_WIDTH
-R_SOLDIER_Y = HEIGHT - R_SOLDIER_HEIGHT
-R_SOLDIER_COLOR = BLUE
-R_SOLDIER_VELOCITY = -5
-
-
+left_tower = Tower(NO_ROTATE)
+right_tower = Tower(ROTATE)
 
 
 def draw_window(left_soldiers_barrack, right_soldiers_barrack): 
@@ -63,18 +36,47 @@ def draw_window(left_soldiers_barrack, right_soldiers_barrack):
     pygame.display.update()
 
 
-def handle_movement(left_soldiers_barrack, right_soldiers_barrack):
-        for left_soldier in left_soldiers_barrack:
-            left_soldier.create_soldier.x += L_SOLDIER_VELOCITY
+def handle_movement(left_soldiers_barrack, right_soldiers_barrack, left_hitbox, right_hitbox):
+    
+    for left_soldier in left_soldiers_barrack:
+        left_soldier.x += velocity
+        if right_soldiers_barrack != []:
+            left_rect = left_soldier.create_hitbox()
+            if right_hitbox.colliderect(left_rect):  # kalo collide dua rectangle
+                print("left collide with right")
+                left_soldiers_barrack.remove(left_soldier)
+            else:
+                print("no collision")
 
-        for right_soldier in right_soldiers_barrack:
-            right_soldier.create_soldier.x += R_SOLDIER_VELOCITY
+    
+    for right_soldier in right_soldiers_barrack:
+        right_soldier.x -= velocity   
+        if left_soldiers_barrack != []:
+            right_rect = right_soldier.create_hitbox()
+            if left_hitbox.colliderect(right_rect):
+                print("right collide with left")
+                right_soldiers_barrack.remove(right_soldier)
+                
+
+            else:
+                print("no collision")
+                   
 
 def main():
     run = True
     clock = pygame.time.Clock()
     left_soldiers_barrack = []
     right_soldiers_barrack = []
+
+    tower_width, tower_height = 100, 200
+    
+    soldier_width, soldier_height = 40, 40
+    soldier_y = HEIGHT - soldier_height
+    left_soldier_x = 20 + tower_width
+    right_soldier_x = WIDTH - 20 - tower_width - soldier_width
+    
+    left_hitbox = pygame.Rect(left_soldier_x, soldier_y, soldier_width, soldier_height)
+    right_hitbox = pygame.Rect(right_soldier_x, soldier_y, soldier_width, soldier_height)
     while run:
         clock.tick(FPS)
         WINDOW.fill(WHITE)
@@ -86,14 +88,17 @@ def main():
 
             if event.type == pygame.KEYDOWN: 
                 if event.key == pygame.K_1:  # summon left side soldier
-                    left_soldier = Soldier(L_SOLDIER_WIDTH, L_SOLDIER_HEIGHT, L_SOLDIER_X, L_SOLDIER_Y, L_SOLDIER_COLOR)
+                    left_soldier = Soldier(left_hitbox.x, left_hitbox.y, soldier_width, soldier_height, NO_ROTATE)
                     left_soldiers_barrack.append(left_soldier)
+                    
+
                 if event.key == pygame.K_LEFT:  # summon right side soldier
-                    right_soldier = Soldier(R_SOLDIER_WIDTH, R_SOLDIER_HEIGHT, R_SOLDIER_X, R_SOLDIER_Y, R_SOLDIER_COLOR)
+                    right_soldier = Soldier(right_hitbox.x, right_hitbox.y, soldier_width, soldier_height, ROTATE)
                     right_soldiers_barrack.append(right_soldier)
 
-        handle_movement(left_soldiers_barrack, right_soldiers_barrack)
+        handle_movement(left_soldiers_barrack, right_soldiers_barrack, left_hitbox, right_hitbox)
         draw_window(left_soldiers_barrack, right_soldiers_barrack)
+        
 
 
     main()
