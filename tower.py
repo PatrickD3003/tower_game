5d40b80776144f4ba1c81a20c17e6193de99830e
NathanaelIanGunadi <38781078+NathanaelIanGunadi@users.noreply.github.com>
2022-09-29 00:14:50 +0900

    Add files via upload

--- tower.py
+++ tower.py
@@ -1,20 +1,26 @@
 import pygame
 import os
-class Tower:
-    WIDTH, HEIGHT = 1200, 500
-    WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-    def __init__(self, tower_rotate):
-        self.width, self.height = 200, 200 
-        self.tower_image_import = pygame.image.load(os.path.join("buildings", "castle.png"))
-        self.tower_image = pygame.transform.scale(self.tower_image_import, (self.width, self.height))
-        self.y_pos = self.HEIGHT - self.tower_image.get_height()
-        if tower_rotate == True:  # if tower di kanan
-            self.x_pos = self.WIDTH - self.tower_image.get_width() - 20
-        else:
-            self.x_pos = 20
-        
-    
-    def draw_tower(self):
-        self.WINDOW.blit(self.tower_image, (self.x_pos, self.y_pos))
-        
+from level import  *
 
+
+class Tower(pygame.sprite.Sprite):
+
+    def __init__(self, team):
+        super().__init__()
+        WIDTH, HEIGHT = pygame.display.get_surface().get_size()
+        WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.width, self.height = WIDTH / 6, HEIGHT / 3
+        self.tower_image_import = pygame.image.load(os.path.join("textures", "castle.png"))
+        self.image = pygame.transform.scale(self.tower_image_import, (self.width, self.height))
+        self.rect = self.image.get_rect()
+
+        self.rect.y = HEIGHT - (self.image.get_height() + tile_size)
+        if team == "1":
+            self.rect.x = round(tile_size * 1.5) - (self.image.get_width() / 2)
+        elif team == "2":
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.rect.x = total_width - round(tile_size * 1.5) - (self.image.get_width() / 2)
+
+    def draw_tower(self, scroll):
+        WIDTH, HEIGHT = pygame.display.get_surface().get_size()
+        pygame.display.get_surface().blit(self.image, (self.rect.x + scroll, self.rect.y))
