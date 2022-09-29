4b746415b81cc8ac64f6c2869f85743650674bb9
PatrickD3003 <111837275+PatrickD3003@users.noreply.github.com>
2022-09-29 10:33:23 +0900

    Update tower.py

--- tower.py
+++ tower.py
@@ -8,15 +8,13 @@ class Tower:
         self.tower_image_import = pygame.image.load(os.path.join("buildings", "castle.png"))
         self.tower_image = pygame.transform.scale(self.tower_image_import, (self.width, self.height))
         self.y_pos = self.HEIGHT - self.tower_image.get_height()
-        
-
         if tower_rotate == True:  # if tower di kanan
             self.x_pos = self.WIDTH - self.tower_image.get_width() - 20
         else:
             self.x_pos = 20
+        
     
     def draw_tower(self):
         self.WINDOW.blit(self.tower_image, (self.x_pos, self.y_pos))
         
-
 
