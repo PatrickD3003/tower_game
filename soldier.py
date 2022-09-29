9953d8c8492ce217243bff69a12cd218c02c13f5
PatrickD3003 <111837275+PatrickD3003@users.noreply.github.com>
2022-09-29 14:34:17 +0900

    Update soldier.py

--- soldier.py
+++ soldier.py
@@ -1,40 +1,42 @@
 import pygame
 import os
-WIDTH, HEIGHT = 1200, 500
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+
 # sementara pake ini dulu
 tower_width = 100
 tower_height = 200
-dx = 0
-dy = 0
 
 class Soldier(pygame.sprite.Sprite):
-    width, height = 40, 40
-
-    def __init__(self, soldier_rotate):
+    
+    def __init__(self, soldier_rotate, window):
         super().__init__()
-        self.width, self.height = 40, 40
+        # input variable
+        self.window = window
         self.rotate = soldier_rotate
+        self.width, self.height = 40, 40    
+        # flags
+        self.alive = True
+        self.collide = False
+        # object creation
         self.image_soldier_import = pygame.image.load(os.path.join("characters", "ogre_sprite.png"))
         self.image_soldier = pygame.transform.scale(self.image_soldier_import, (self.width, self.height))
         self.rect = self.image_soldier.get_rect()
-        self.rect.y = HEIGHT - self.height
-        self.alive = True
+
+        self.rect.y = self.window.get_height() - self.height
         if self.rotate == True:  # kalo misalkan kubu sebelah kanan
             self.image_soldier = pygame.transform.flip(self.image_soldier, True, False)
-            self.rect.x = WIDTH - 20 - tower_width - self.width
+            self.rect.x = self.window.get_width() - 20 - tower_width - self.width
         else:
             self.rect.x = 20 + tower_width
  
     def summon_soldier(self):
-        WINDOW.blit(self.image_soldier, (self.rect.x, self.rect.y))
+        self.window.blit(self.image_soldier, (self.rect.x, self.rect.y))
 
-    
     def move_soldier(self):
+        dx, dy = 5, 0
         if self.rotate == True:
-            dx, dy = -5, 0
+            dx *= -1
         else:
-            dx, dy = 5, 0
+            dx *= 1
         self.rect.move_ip(dx, dy)
     
     def stop_soldier(self):
@@ -42,18 +44,14 @@ class Soldier(pygame.sprite.Sprite):
         dy = 0
         self.rect.move_ip(dx, dy)
 
-
-    def collide(self, enemy_group, enemy_barrack):
+    def collision_detector(self, enemy_group):
         self.enemy_group = enemy_group
-        self.enemy_barrack = enemy_barrack
-
-        if self.enemy_barrack != []:
-            if pygame.sprite.spritecollideany(self, enemy_barrack):
-                self.alive = False
-            else:
-                self.alive = True
+        if pygame.sprite.spritecollideany(self, enemy_group):
+            self.collide = True
         else:
-            return True
+            self.collide = False
+
+
         
 
 
@@ -65,4 +63,3 @@ class Soldier(pygame.sprite.Sprite):
 
     
     
-
