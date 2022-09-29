e943159a934b2794e56a53d3c43d6db87eaa0405
PatrickD3003 <111837275+PatrickD3003@users.noreply.github.com>
2022-09-29 14:34:39 +0900

    Update tower.py

--- tower.py
+++ tower.py
@@ -1,20 +1,20 @@
 import pygame
 import os
-class Tower:
-    WIDTH, HEIGHT = 1200, 500
-    WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-    def __init__(self, tower_rotate):
+class Tower(pygame.sprite.Sprite):
+
+    def __init__(self, tower_rotate, window):
+        super().__init__()
+        self.window = window
         self.width, self.height = 200, 200 
         self.tower_image_import = pygame.image.load(os.path.join("buildings", "castle.png"))
         self.tower_image = pygame.transform.scale(self.tower_image_import, (self.width, self.height))
-        self.y_pos = self.HEIGHT - self.tower_image.get_height()
+        self.rect = self.tower_image.get_rect()
+        self.rect.y = self.window.get_height() - self.tower_image.get_height()
         if tower_rotate == True:  # if tower di kanan
-            self.x_pos = self.WIDTH - self.tower_image.get_width() - 20
-        else:
-            self.x_pos = 20
+            self.rect.x = self.window.get_width() - self.tower_image.get_width() - 20
+        elif tower_rotate == False:
+            self.rect.x = 20
         
     
     def draw_tower(self):
-        self.WINDOW.blit(self.tower_image, (self.x_pos, self.y_pos))
-        
-
+        self.window.blit(self.tower_image, (self.rect.x, self.rect.y))
