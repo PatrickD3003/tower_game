1e0535e0016f600d1b70213ef6d9f2f40fb05413
NathanaelIanGunadi <38781078+NathanaelIanGunadi@users.noreply.github.com>
2022-10-14 11:55:29 +0900

    projectile

---
+++ projectile.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Projectile(pygame.sprite.Sprite):
+
+    def __init__(self, img, width, height, velocity):
+        super(Projectile, self).__init__()
+        self.xv = velocity[0]
+        self.yv = velocity[1]
+        self.image = pygame.image.load(img)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image
+
+    def summon(self, unit):
+        self.unit = unit
+        self.team = unit.team
+        self.dmg = unit.dmg
+        if self.team == "1":
+            self.rect.x = self.unit.rect.x + self.unit.rect.get_width()
+        else:
+            self.rect.x = self.unit.rect.x
+
+        self.rect.y = self.unit.rect.y - 10
+
+    def move(self):
